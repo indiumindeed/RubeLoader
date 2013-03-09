845db2de59b26b38c5cd3fa31e5c6ae47968af2c
Tim Scott <tescott425@gmail.com>
2013-03-09 10:56:03 -0600

    Changed up Vector2 serializer to eliminate try/catch which was emitting several warnings on DalvikVM.

--- RubeLoader/src/com/mangecailloux/rube/loader/serializers/Vector2Serializer.java
+++ RubeLoader/src/com/mangecailloux/rube/loader/serializers/Vector2Serializer.java
@@ -3,7 +3,7 @@ package com.mangecailloux.rube.loader.serializers;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.Json.ReadOnlySerializer;
-import com.badlogic.gdx.utils.SerializationException;
+import com.badlogic.gdx.utils.OrderedMap;
 
 public class Vector2Serializer extends ReadOnlySerializer<Vector2>
 {
@@ -11,14 +11,9 @@ public class Vector2Serializer extends ReadOnlySerializer<Vector2>
 	@Override
 	public Vector2 read(Json json, Object jsonData, Class type) 
 	{
-		Vector2 vector = new Vector2(0.0f, 0.0f); 
+		Vector2 vector = new Vector2(); 
 		
-		try 
-		{
-			// consume "null" vectors here.
-			json.readValue(float.class, jsonData);
-		} 
-		catch (SerializationException e) 
+		if (jsonData instanceof OrderedMap)
 		{
 			vector.x = json.readValue("x", float.class, 0.0f, jsonData);
 			vector.y = json.readValue("y", float.class, 0.0f, jsonData);
